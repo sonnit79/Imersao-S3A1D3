c6a13ffc85eb4b8146e7d76a3ef4a09849fe7417
sonnit79 <richardsonnitz@yahoo.com.br>
2026-02-24 00:53:47 -0300

    Codigo inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Imersao-3SA1D3';
+
+  posts = signal([
+    {titulo: 'Postagem 1', conteudo: 'Detalhes da postagem 1'},
+    {titulo: 'Postagem 2', conteudo: 'Detalhes da postagem 2'},
+  ]);
+
+  novoTitulo = signal('');
+  novoConteudo = signal('');
+
+  postEditado = signal<any>(null);
+
+  adicionarPost(){
+    if(this.novoTitulo() && this.novoConteudo()){
+      this.posts.update((lista) => [
+        { titulo: this.novoTitulo(), conteudo: this.novoConteudo() },
+        ...lista
+      ]);
+      this.novoTitulo.set('');
+      this.novoConteudo.set('');
+    }else{
+      alert('Preencha todos campos.');
+    }
+  }
+
+  excluirPost(postRemover: any){
+    this.posts.update((listaAtual) => listaAtual.filter(post => post !== postRemover));
+  }
+
+  editarPost(post: any){
+    this.postEditado.set(post);
+    this.novoTitulo.set(post.titulo);
+    this.novoConteudo.set(post.conteudo);
+  }
+
+  salvarEdicao(){
+    this.posts.update((lista) => {
+      return lista.map(p => {
+        if (p === this.postEditado()) {
+          return { titulo: this.novoTitulo(), conteudo: this.novoConteudo() };
+        }
+        return p;
+      });
+    });
+  }
+
+  cancelarEdicao(){
+    this.postEditado.set(null);
+    this.novoTitulo.set('');
+    this.novoConteudo.set('');
+  }
 }
